01c5990ded8ee41fdad9a6648ec7cd6236e9a9f3
Martin Alejandro Escobar Espinel <martinale4@hotmail.com>
2021-08-25 22:10:01 -0500

    final taller

--- 2180194_Escobar_Martin/taller/static/js/work.js
+++ 2180194_Escobar_Martin/taller/static/js/work.js
@@ -22,16 +22,6 @@ $(document).ready(() => {
 
     }
 
-    var controller = new ScrollMagic.Controller();
-
-    new ScrollMagic.Scene({
-        duration: 100,
-        offset: 50
-    })
-        .setPin("#my-sticky-element")
-        .addTo(controller);
-
-
 });
 
 function handleMove(e, el) {
