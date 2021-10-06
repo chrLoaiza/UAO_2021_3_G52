c5c46e11af01d586cb17c74ea99b6785c107e1f3
Eliot27 <eliot27@live.com>
2021-10-06 17:45:50 -0500

    Resolviendo Problemas

--- 2127210_Gomez_Eliot/05_Parcial_2/logic/index.js
+++
@@ -1,7 +0,0 @@
-const request_btn = document.getElementById("request-btn");
-request_btn.addEventListener("click", async () => {
-  const cardList = document.getElementById("card-list");
-  const response = await fetch(
-    //realizar petición web
-  );
-});
