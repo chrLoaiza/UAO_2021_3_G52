a71459c8d4d563e7cb29d7c6b29e37fed82936bc
v1nay3010 <angie_van.fajardo@uao.edu.co>
2021-10-11 10:32:07 -0500

    ejercicios

--- 2190229_Angie_Fajardo/Ejericcio_2_JS_async/javascript/index.js
+++ 2190229_Angie_Fajardo/Ejericcio_2_JS_async/javascript/index.js
@@ -6,3 +6,34 @@
 document
   .getElementById("apiButton")
   .addEventListener("click", this.generateUsers.bind(this));
+
+function generateUsers() {
+  const userAll = this.document.getElementById("users");
+  userAll.innerHTML = "";
+  this.fetchUsersData().then((data) => {
+    const users = data.results;
+    for (let index = 0; index < users.length; index++) {
+      const element = users[index];
+      const userElement = `
+    <div class="user-container">
+    <div class="user-header">
+    <img src="${element.picture.large}" alt="medium_profile">
+    </div>
+    <div class="user-body">
+    <h3>${element.name.title}. ${element.name.first} ${element.name.last}</h3>
+    <p>Age: ${element.registered.age} years old</p>
+    <p>Gender: ${element.gender}</p>
+    <p>Email: ${element.email}</p>
+    <p> Phone: ${element.phone}</p>
+    <p> Location: ${element.location.city}, ${element.location.state} (${element.location.country})</p>
+    </div>
+    </div>`;
+      userAll.innerHTML += userElement;
+    }
+  });
+}
+
+async function fetchUsersData() {
+  const response = await fetch("https://randomuser.me/api/?results=5");
+  return response.json();
+}
