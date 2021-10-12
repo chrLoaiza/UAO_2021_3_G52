5e450751597e8d00fa01a311ef981264e80d53d1
Juan Pablo Rivera González <68454373+JuanPRivera@users.noreply.github.com>
2021-10-11 21:56:12 -0500

    PR 2180195_Juan_Rivera (#41)
    * Add Parcial_1 directory
    
    * my exam
    
    * js practice
    
    * js fixed
    
    * async activity
    
    * pokeParcial 2
    
    Co-authored-by: Christian Loaiza Garcia <chr.loaiza@gmail.com>

---
+++ 2180195_Juan_Rivera/Ejericcio_1_JS/javascript/index.js
@@ -0,0 +1,37 @@
+// const functionBtn = document.getElementById("namedFunction");
+
+// functionBtn.addEventListener("click", this.buttonClicked);
+
+let inputs = document.getElementsByClassName('input');
+
+for(let i = 0; i < inputs.length; i++){
+  inputs[i].addEventListener('focus', (event) => {
+    event.target.style.border = '2px solid rgb(34, 32, 155)';
+  });
+
+  inputs[i].addEventListener('blur', (event) => {
+    event.target.style.border = '1px solid rgb(0, 238, 255)';
+  });
+}
+
+function dobleClick(event) {
+  // alert('I was double clicked, yaaay!!!!!!!!!');
+}
+
+function buttonClicked(event) {
+  // alert("Normal Function");
+}
+
+function formSubmited(event) {
+  let element = document.getElementById('form').children
+  const loopSize = element.length;
+  let mailData = {}
+  for (let index = 0; index < loopSize; index++) {
+    const element = document.getElementById('form').children[index];
+    const inputEle = element.children[1];
+    mailData[inputEle.name] = inputEle.value;
+  }
+
+  console.log(mailData);
+  alert('Datos subidos correctamente :D!!!');
+}
