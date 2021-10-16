d6cc85eea75d2d82494d03264b9eb26b6016ba7b
Ashley Hélago <ashley.hnlago@uao.edu.co>
2021-10-15 22:51:08 -0500

    Events workshop

---
+++ 2167660_Ashley_Helago/Tarea_HTML/PaginaWeb/logic.js
@@ -0,0 +1,17 @@
+//Aviso emergente
+function fSubmited(event){
+    alert('Se ha registrado un nuevo visitante !!!')
+}
+
+//logic input
+function focusElement(event){
+    const element = event.srcElement;
+    element.classList.toggle('FocElemt',true);
+}
+
+//Blur "output"
+function blurElement(event){
+    const element = event.srcElement;
+    element.classList.toggle('FocElemt',false);
+    event.srcElement.value = event.srcElement.value.toUpperCase()
+}
